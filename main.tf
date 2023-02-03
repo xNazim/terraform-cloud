14b31fbfd37817bf14e24cc6630d0883abb7c10a
Nazim <nazim.muhtarbekov73@gmail.com>
2023-02-03 09:15:55 +0600

    first commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform"
+  location = "West Us"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Test"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "nazim-network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["192.168.0.0/16"]
+}
+
+
+
+resource "azurerm_subnet" "subnets" {
+  count                = length(var.subnets)
+  name                 = var.subnets[count.index]
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.prefixes[count.index]]
+
+}
+
+
+resource "azurerm_network_interface" "nic" {
+  count               = length(var.nic)
+  name                = var.nic[count.index]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnets[count.index].id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+}
+
+
+
+resource "azurerm_subnet_network_security_group_association" "nsg-subnet" {
+  count                     = length(var.subnets)
+  subnet_id                 = azurerm_subnet.subnets[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "subnets" {
+  description = "Subnets"
+  type = list(string)
+  default = ["subnet1", "subnet2", "subnet3"]
+}
+
+variable "prefixes" {
+  description = "subnet address prefixes"
+  type = list(string)
+  default = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24"]
+}
+
+variable "nic" {
+  description = "nic subnet attachment"
+  type = list(string)
+  default = [ "nic1", "nic2", "nic3" ]
+}
+
